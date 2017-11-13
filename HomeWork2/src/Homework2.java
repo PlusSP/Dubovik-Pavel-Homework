569aab4fbd929097432213de9326461d72426431
Pavel Dubovik <33573891+PlusSP@users.noreply.github.com>
2017-11-14 01:36:26 +0300

    домашка

---
+++ HomeWork2/src/Homework2.java
@@ -0,0 +1,10 @@
+public class Homework2 {
+    public static void main(String[] args){
+     int x=2;
+     String text = "string1";
+     String text1 = text.substring(2,3);
+     System.out.println(text1);
+
+
+     }
+}
